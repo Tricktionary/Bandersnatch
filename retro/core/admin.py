2d18bf774c5d8cadc74f7fed2bbb7b071cd5840f
Tricktionary <andyaoux@hotmail.com>
2019-02-02 19:34:28 -0500

    Setup modeal

--- retro/core/admin.py
+++ retro/core/admin.py
@@ -5,3 +5,15 @@ from django.contrib.auth.models import User
 @admin.register(Game)
 class Game(admin.ModelAdmin):
     pass
+
+@admin.register(Player)
+class Player(admin.ModelAdmin):
+    pass
+
+@admin.register(Stage)
+class Stage(admin.ModelAdmin):
+    pass
+
+@admin.register(Choice)
+class Choice(admin.ModelAdmin):
+    pass
--- retro/core/models.py
+++ retro/core/models.py
@@ -1,14 +1,27 @@
 from django.conf import settings
+from django import forms
 from django.db import models
 from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
 
-class Game(models.Model):
-    score = models.IntegerField(blank=True, default=0)
-
-    def increaseScore(self,score):
-        self.score += score
-        self.save()
+class Player(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.CharField(max_length=400)
 
     def __str__(self):
-        return str(self.id)
+        return self.player.email
+
+class Game(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    stage = models.ManyToManyField('Stage', blank=True)
+    end = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.player.email
+
+class Stage(models.Model):
+    description = models.CharField(max_length=400)
+    choices = models.ManyToManyField('Choice', blank=True)
+
+class Choice(models.Model):
+    description = models.CharField(max_length=200)
